9df744f6c547acea26b6364af4fb8bbfbdb4090d
Marian <hellomariandev@gmail.com>
2022-02-24 21:58:54 +0100

    getting blog post data with Next.js

--- blog_next/pages/index.js
+++ blog_next/pages/index.js
@@ -1,8 +1,22 @@
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ posts, title }) {
+  console.log(posts);
+
   return (
     <>
-      Home
+      <h2>{posts[0].attributes.title}</h2>
+      <h2>{posts[1].attributes.title}</h2>
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const postsRes = await axios.get("http://localhost:1337/api/posts");
+
+  return {
+    props: {
+      posts: postsRes.data.data,
+    },
+  };
 }
